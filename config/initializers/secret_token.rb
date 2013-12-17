56fbf9da70656a6f0add5119a97583d27dab1931
quynhbv <quynhbv.hp@gmail.com>
2013-12-17 14:13:06 +0700

    Chatwork_fake

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ChatworkFake::Application.config.secret_key_base = 'd290f2bf048c306f5c4965e8e80304f3d23a192fca35d68d1d74418d17eaec095767b989ca7fba29c4271d50ccba1fb0cec70a4e6327cfe3d2487d34ca505fa8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ChatworkFake::Application.config.session_store :cookie_store, key: '_chatwork_fake_session'
