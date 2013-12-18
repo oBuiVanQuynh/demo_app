fd58136fdc662937c66b5ce71fc513b5503024de
Your Name <your.email@example.com>
2013-12-18 15:58:18 +0700

    ajax comment

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,4 +3,5 @@ class Message < ActiveRecord::Base
   default_scope -> {order("created_at DESC")}
   validates :content, presence: true, length: {maximum: 1000}
   validates :user_id, presence: true
+  scope :chat_with_friend, ->user_id,friend_id{where"((user_id = :user_id and friend_id = :friend_id) OR (user_id = :friend_id and friend_id = :user_id))",user_id: user_id, friend_id: friend_id}
 end
