fa2b508858dc4ae02b93d5a4ea7cbdf49390a692
quynhbv <quynhbv.hp@gmail.com>
2013-12-19 16:34:10 +0700

    Chat

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,4 +15,5 @@
 //= require jquery.ui.all
 //= require bootstrap
 //= require turbolinks
+//= require websocket_rails/main
 //= require_tree .
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,15 +1,57 @@
 class MessagesController < ApplicationController
 
-  def new
+  def show
+    @receiver = User.find(params[:id])
+    @histories = @receiver.histories_with(current_user).paginate(:page => params[:page], :order => 'created_at')
+    respond_to do |format|
+      format.html { render :partial => "shared/histories", :histories => @histories, :receiver => @receiver }
+      format.js {
+        render 'histories/show'
+      }
+    end
   end
 
   def create
-    user = User.find params[:user_id]
-    @message = current_user.messages.build message_params
-    @message.update_attributes(friend_id: user.id) 
-    respond_to do |format|
-        format.html
-        format.js
+    @message = current_user.messages.new(message_params)
+    if @message.save
+      # respond_to do |format|
+      #   format.html { render :partial => "histories/message", :message => @message }
+      #   format.js {
+      #     render 'histories/create'
+      #   }
+      # end
+      #user_msg :new_message, message[:msg_body].dup
+      logger.info "----------start----------"
+      logger.info @message.content
+      event = WebsocketRails::Event.new(:new_message, data: {
+        user_name:  @message.user_id, 
+        received:   Time.now.to_s(:short), 
+        msg_body:   ERB::Util.html_escape(@message.content) 
+      })
+      #WebsocketRails["system"].trigger event
+      #binding.pry
+      # WebsocketRails.users[@message.receiver.id.to_s].send_message :new_message, { 
+      #WebsocketRails.users[@message.receiver.id.to_s].trigger event
+      WebsocketRails.users.each do |connection|
+        event = WebsocketRails::Event.new(:new_message, data: {
+          user_name:  @message.user_id, 
+          received:   Time.now.to_s(:short), 
+          msg_body:   ERB::Util.html_escape(@message.content) 
+        })
+        connection.trigger event
+      end
+      # WebsocketRails.broadcast_message Event.new(:new_message, data: {
+      #   user_name:  @message.poster.name, 
+      #   received:   Time.now.to_s(:short), 
+      #   msg_body:   ERB::Util.html_escape(@message.content) 
+      # })
+      # WebsocketRails.users.send_message :new_message, { 
+      # user_name:  @message.poster.name, 
+      # received:   Time.now.to_s(:short), 
+      # msg_body:   ERB::Util.html_escape(@message.content) 
+      # }
+      #WebsocketRails[@message.receiver.email].trigger :new_message, @message.content
+      logger.info "----------end----------"
     end
   end
 
@@ -19,6 +61,6 @@ class MessagesController < ApplicationController
   private
 
     def message_params
-      params.require(:message).permit(:content)
+      params.require(:message).permit(:content, :friend_id)
     end
 end
